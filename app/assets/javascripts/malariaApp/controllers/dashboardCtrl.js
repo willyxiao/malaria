cb02b8e0f4decf294c2bb34c90407a43d2129129
Willy Xiao <willy@chenxiao.us>
2016-02-16 03:55:51 +0000

    picture'

--- app/assets/javascripts/malariaApp/controllers/dashboardCtrl.js
+++ app/assets/javascripts/malariaApp/controllers/dashboardCtrl.js
@@ -1,13 +1,13 @@
 ! function(angular) {
+    var ENDPOINT_URL = '/api/user'
 
     var malariaCtrls = angular.module('malariaCtrls', ['maDirectives', 'maServices']);
     malariaCtrls.controller("DashboardCtrl", ["$scope", "$http", "maUser", function($scope, $http, maUser) {
         $scope.startDate = new Date("Mon Apr 11 2016 03:00:00 GMT-0500 (EST)");
-        maUser.then(function(user) {
-            $scope.user = user;
-            debugger;
+        $http.get(ENDPOINT_URL).then(function(response) {
+            $scope.user = response.data;
+        }).catch(function(err) {
+            console.error(err);
         });
-        
-        
     }]);
 }(angular);
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,7 @@ class ApiController < ApplicationController
     def user
         render json: {
             id: current_user.id,
+            uid: current_user.uid,
             name: current_user.name,
             email: current_user.email,
             confirmed_email: current_user.confirmed_email,
