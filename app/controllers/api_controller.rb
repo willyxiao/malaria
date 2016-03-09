021d4d882c8cb43e33db711441f46a71e9ec223b
Willy Xiao <willy@chenxiao.us>
2016-03-09 05:15:54 +0000

    update game

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,6 +7,8 @@ class ApiController < ApplicationController
             email: current_user.email,
             confirmed_email: current_user.confirmed_email,
             image: current_user.image,
+            community: current_user.community.name,
+            school: current_user.community.school.name,
         }
     end
 end
