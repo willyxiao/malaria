7a123b61fef9d5f5d30f2965bfa32a92003dfc2e
Willy Xiao <willy@chenxiao.us>
2016-04-10 18:37:11 +0000

    facts

--- app/models/player.rb
+++ app/models/player.rb
@@ -7,26 +7,27 @@ class Player < ActiveRecord::Base
     
     has_many :killstories
 
-    KILL_PAUSE = 5
-
     def self.create_player(user, game, community)
         self.create!(community: community, game: game, user: user)
     end
     
     def dead?
         (Killstory.where(game: self.game, dead_id: self.id).count > 0) 
-        # and 
-        # KILL_PAUSE < ((Time.now - Killstory.where(game: self.game, dead_id: self.id).take.) / 60)
     end
     
     def alive?
         not self.dead?
     end
     
+    def kill_player_debug
+        Killstory.submit_kill(self.assassin, self, true, "Debug")
+        a = self.assassin
+        a.target = self.target
+        a.save
+    end
+    
     def paused_time?
         false
-        # self.killstories.where > 0 and 
-        # KILL_PAUSE < ((Time.now - self.killstory) / 60)
     end
     
     def submitted_death?
@@ -35,5 +36,9 @@ class Player < ActiveRecord::Base
     
     def death_story
         Killstory.where(is_kill_story: false, dead_id: self.id).take
+    end
+    
+    def assassin
+        Player.where(target_id: self.id).take
     end
 end
