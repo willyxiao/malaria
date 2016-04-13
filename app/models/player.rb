06766fdb285dbca32226fde9cf48f8586ad1e3ac
Willy Xiao <willy@chenxiao.us>
2016-04-13 20:21:50 +0000

    change player

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,8 +2,7 @@ class Player < ActiveRecord::Base
     belongs_to :game
     belongs_to :user
     belongs_to :community
-    has_one :target, class_name: "Player", foreign_key: "target_id"
-    # belongs_to :target, class_name: "Player"
+    belongs_to :target, class_name: "Player"
     
     has_many :killstories
 
