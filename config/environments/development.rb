685c01ecc18ce7a52510c6263db028d9cbe041a9
Kaveh Khorram <kkhorram@gmail.com>
2018-03-31 16:10:27 -0700

    first push to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => "https://CHANGEME.THOUGH" }
+  config.action_mailer.default_url_options = { :host => "127.0.0.1", :port => 9292 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
